9267f0341ffdddbd0378ca696c6f731db90ea82e
harshg0910 <harshg0910@gmail.com>
2013-03-28 03:00:44 -0700

    trying to use database. Some errors are there.

---
+++ CampusEvents/src/iitg/campusevents/Events.java
@@ -0,0 +1,17 @@
+package iitg.campusevents;
+
+import java.util.Date;
+
+public class Events {
+	long eventId;
+	Date startTime;
+	Date endTime;
+	Date modifiedTime;
+	String title;
+	String content;
+	long locationId;
+	long categoryId;
+	long postedBy;
+	String status;
+	
+}
